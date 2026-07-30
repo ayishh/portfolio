6e3a7905569b5b458f5250287bacc0cab0f2bf0b
Fariez Daniel <fariezdaniel03@gmail.com>
2026-07-30 09:06:40 -0400

    remove navbar and darkmode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -41,24 +41,6 @@ function App() {
   return (
     <div className="App">
       <div className="nav-container">
-        <DarkModeToggle />
-        <PillNav
-          logo={logo}
-          logoAlt="Logo"
-          items={[
-            { label: 'Home', href: '/' },
-            { label: 'About', href: '/about' },
-            { label: 'Gallery', href: '/services' },
-            { label: 'Contact', href: '/contact' }
-          ]}
-          activeHref="/"
-          className="custom-nav"
-          ease="power2.easeOut"
-          baseColor="#000000"
-          pillColor="#ffffff"
-          hoveredPillTextColor="#ffffff"
-          pillTextColor="#000000"
-        />
       </div>
 
       <div className="projects-section">
